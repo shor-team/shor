a19b7687c7e8df558cdd68a5db278d024b838355
Collin Overbay - shor.dev <shordotdev@gmail.com>
2020-02-18 22:36:22 -0800

    First release of shor library 0.0.1

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = []
+
+setup(
+    name="shor",
+    version="0.0.1",
+    author="shor.dev",
+    author_email="shordotdev@gmail.com",
+    description="Quantum Computing for Humans",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/shor-team/shor",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
---
+++ shor/simulation.py
@@ -0,0 +1,9 @@
+
+class QuantumProgram:
+
+    def __init__(self):
+        # Work to be done :)
+        print('Hello World')
+
+    def run(self):
+        print('Running')
