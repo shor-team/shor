0f0465b826aefd2161d4921840dc75eb49d7a210
Collin Overbay - shor.dev <shordotdev@gmail.com>
2020-03-01 10:40:02 -0800

    Add integration tests for basic 1 and 2 qubit circuits

---
+++ tests/integration/test_basic_examples.py
@@ -0,0 +1,46 @@
+def test_single_qubit():
+    from shor.quantum import Circuit
+    from shor.gates import Hadamard
+
+    circuit = Circuit(qbits=[0])
+    circuit.add(Hadamard)
+
+    from shor.backends import QuantumSimulator
+    from shor.quantum import QSession
+
+    sess = QSession(backend=QuantumSimulator)
+    result = sess.run(circuit, num_shots=1024)
+
+    # Accounting for random noise, results won't be exact
+    assert result.counts[0] > 450
+    assert result.counts[1] > 450
+
+
+def test_entanglement():
+    from shor.quantum import Circuit
+    from shor.gates import CNOT, Hadamard
+    from shor.gates import Qubits
+    from shor.operations import Measure
+
+    circuit = Circuit()
+    circuit.add(Qubits(2))
+    circuit.add(Hadamard(0))
+    circuit.add(CNOT(0, 1))
+    circuit.add(Measure(0, 1))
+
+    from shor.backends import QuantumSimulator
+    from shor.quantum import QSession
+
+    sess = QSession(backend=QuantumSimulator)
+    result = sess.run(circuit, num_shots=1024)
+
+    # Accounting for random noise, results won't be exact
+    # For result index:
+    #     - int('10',2)
+    #     - 0b10
+    #     - 2
+    # are all equivalant ways to get the index for '|10>'
+    assert result.counts[0b01] == 0
+    assert result.counts[0b10] == 0
+    assert result.counts[0b00] > 450
+    assert result.counts[0b11] > 450
