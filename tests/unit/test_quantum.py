4fd058b1e5b0ad15d2db20016978138c5296f5ef
Collin Overbay - shor.dev <shordotdev@gmail.com>
2020-03-01 21:15:50 -0800

    TEST: Add tests for Circuit.add (Gate and Layer)

--- tests/unit/test_quantum.py
+++ tests/unit/test_quantum.py
@@ -1,13 +1,26 @@
 def test_circuit_init():
     from shor.quantum import Circuit
-    circuit = Circuit()
+    Circuit()
 
 
-def test_circuit_add():
+def test_circuit_add_layer():
     from shor.quantum import Circuit
-    from shor.layers import Qubits
+    from shor.layers import _Layer
     circuit = Circuit()
-    circuit.add(Qubits(3))
+    prev_num_layers = len(circuit.layers)
+    circuit.add(_Layer())
+
+    assert len(circuit.layers) == prev_num_layers + 1
+
+
+def test_circuit_add_gate():
+    from shor.quantum import Circuit
+    from shor.gates import _Gate
+    circuit = Circuit()
+    prev_num_gates = len(circuit.gates)
+    circuit.add(_Gate())
+
+    assert len(circuit.gates) == prev_num_gates + 1
 
 
 def test_qsession_init():
