62fe5082a8e5a7df6f0e4e1825008323a64c1f9e
Collin Overbay - shor.dev <shordotdev@gmail.com>
2020-03-30 20:36:23 -0700

    TEST: test for adding / combining quantum circuits

--- tests/unit/test_quantum.py
+++ tests/unit/test_quantum.py
@@ -1,11 +1,12 @@
+from shor.quantum import Circuit
+from shor.layers import _BaseLayer
+
+
 def test_circuit_init():
-    from shor.quantum import Circuit
     Circuit()
 
 
 def test_circuit_add_base_layer():
-    from shor.quantum import Circuit
-    from shor.layers import _BaseLayer
     circuit = Circuit()
     prev_num_layers = len(circuit.layers)
     circuit.add(_BaseLayer())
@@ -14,11 +15,20 @@ def test_circuit_add_base_layer():
 
 
 def test_circuit_add_returns_self():
-    from shor.quantum import Circuit
-    from shor.layers import _BaseLayer
 
     circuit1 = Circuit()
     circuit2 = circuit1.add(_BaseLayer())
 
     assert circuit1 == circuit2
 
+
+def test_circuit_add_circuit():
+    circuit = Circuit().add(_BaseLayer()).add(_BaseLayer)
+
+    circuit_to_add = Circuit()
+    circuit_to_add.add(_BaseLayer()).add(_BaseLayer).add(_BaseLayer())
+
+    assert len(circuit.layers) == 2
+    circuit.add(circuit_to_add)
+
+    assert len(circuit.layers) == 5
