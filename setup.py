6a09b9ec1f505385e4f4de03900467c455c625fa
Collin Overbay - shor.dev <shordotdev@gmail.com>
2020-05-12 20:45:30 -0700

    RELEASE: Released alpha v0.0.2a2

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
 
-requirements = []
+requirements = ['numpy>=1.9.1']
 
 setup(
     name="shor",
-    version="0.0.2a1",
+    version="0.0.2a2",
     author="shor.dev",
     author_email="shordotdev@gmail.com",
     description="Quantum Computing for Humans",
@@ -15,6 +15,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/shor-team/shor",
     packages=find_packages(),
+    python_requires='>=3.6',
     install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3.7",
