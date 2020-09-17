7d44c3d1d20cb2ca4f7318d403121c0b0bdf775e
Collin Overbay - shor.dev <shordotdev@gmail.com>
2020-09-16 17:24:00 -0700

    FEATURE (issues/29): Add IBMQ / Qiskit provider support, removing backend in favor of 'Provider' class.

--- shor/providers/ShorSimulator.py
+++ shor/providers/ShorSimulator.py
@@ -1,11 +1,15 @@
 from collections import deque, Counter
 from functools import lru_cache
-from typing import List, Deque, NamedTuple, Tuple, Dict
+from typing import NamedTuple, List, Tuple, Deque, Dict
 
 import numpy as np
 
 from shor.gates import _Gate
-from shor.quantum import Circuit
+from shor.providers import Provider, Result, Job
+from shor.providers.base import JobStatus, JobStatusCode
+from shor.quantum import Circuit, QuantumCircuit
+from shor.utils.qbits import has_common_qbits, change_qubit_order, get_entangled_initial_state, int_from_bit_string, \
+    int_to_bit_string
 
 
 class _GateTuple(NamedTuple):
@@ -14,12 +18,39 @@ class _GateTuple(NamedTuple):
     order: int
 
 
-class _QuantumBackend(object):
-    def run(self, initial_state: List[int], gates: List[_Gate], measure_bits: List[int], num_shots, **kwargs):
-        pass
+class ShorSimulatorResult(Result):
+    def __init__(self, counts: Dict[int, int], sig_bits: int):
+        self._counts = counts
+        self._sig_bits: int = sig_bits
+
+    @property
+    def counts(self):
+        return self._counts
+
+    @property
+    def sig_bits(self):
+        return self._sig_bits
 
 
-class QuantumSimulator(_QuantumBackend):
+class ShorSimulatorJob(Job):
+
+    def __init__(self, status: JobStatusCode, result: Result):
+        self._status = status
+        self._result = result
+
+    @property
+    def status(self):
+        return self._status
+
+    @property
+    def result(self):
+        return self._result
+
+
+class ShorSimulator(Provider):
+    @property
+    def jobs(self) -> List[Job]:
+        self._jobs
 
     @staticmethod
     @lru_cache(maxsize=10)
@@ -87,100 +118,18 @@ class QuantumSimulator(_QuantumBackend):
 
         return to_combine.pop()
 
-    def run(self, initial_state: List[int], gates: List[_Gate], measure_bits: List[int], num_shots, **kwargs):
-        combined = self.combine_gates(tuple(gates))
+    def run(self, qc: QuantumCircuit, times):
+        combined = self.combine_gates(tuple(qc.to_gates()))
         new_qubit_order = combined.qubits
-        state_vector = get_entangled_initial_state(initial_state, new_qubit_order)
+        state_vector = get_entangled_initial_state(qc.initial_state(), new_qubit_order)
 
         probabilities = np.square(np.absolute(combined.matrix.dot(state_vector))).real
         probabilities /= np.sum(probabilities)
 
-        measured = Counter([np.random.choice(state_vector.shape[0], p=probabilities) for m in range(num_shots)])
-        return measured
+        measured = Counter([np.random.choice(state_vector.shape[0], p=probabilities) for m in range(times)])
 
+        return ShorSimulatorJob(
+            JobStatusCode.COMPLETED,
+            ShorSimulatorResult(measured, len(new_qubit_order))
+        )
 
-class QSession(object):
-    def __init__(self, **kwargs):
-        self.backend = kwargs.get('backend', QuantumSimulator())
-
-    def run(self, circuit: Circuit, **kwargs):
-        num_shots = kwargs.get('num_shots', 1024)
-
-        initial_state = circuit.initial_state()
-        gates = circuit.to_gates()
-        measure_bits = circuit.measure_bits()
-
-        counts = self.backend.run(initial_state, gates, measure_bits, num_shots)
-
-        return QResult(counts, sig_bits=len(circuit.initial_state()))
-
-
-class QResult(object):
-    def __init__(self, counts: Dict[int, int], sig_bits: int):
-        self.counts: Dict[int, int] = counts
-        self.sig_bits: int = sig_bits
-
-    def get(self, key, default=0):
-        if isinstance(key, str):
-            idx = int_from_bit_string(key)
-        else:
-            idx = key
-
-        return self.counts.get(idx, default)
-
-    def __getitem__(self, item):
-        return self.get(item)
-
-    def __repr__(self):
-        return repr({int_to_bit_string(k, self.sig_bits): v for k, v in self.counts.items()})
-
-
-def get_entangled_initial_state(initial_state, new_qubit_order):
-    states_to_entangle: Deque[np.ndarray] = deque()
-
-    states = list(map(lambda s: np.asarray([1 if s == 0 else 0, 1 if s == 1 else 0], dtype='complex64'), initial_state))
-
-    for q in new_qubit_order:
-        states_to_entangle.append(states[q])
-
-    while len(states_to_entangle) > 1:
-        entangled_states: Deque[np.ndarray] = deque()
-        while len(states_to_entangle) > 1:
-            entangled_states.append(np.kron(states_to_entangle.popleft(), states_to_entangle.popleft()))
-        if len(states_to_entangle) == 1:
-            entangled_states.append(states_to_entangle.popleft())
-        states_to_entangle = entangled_states
-
-    return states_to_entangle.pop()
-
-
-def change_qubit_order(matrix: np.ndarray, old_order, new_order) -> np.ndarray:
-    reorder_qubits = [new_order.index(q2) for q2 in old_order]
-    initial_row_order = [i for i in range(np.power(2, len(old_order)))]
-
-    reorder_rows = list(map(lambda i: rearrange_bits(i, reorder_qubits), initial_row_order))
-
-    return matrix[reorder_rows][:, reorder_rows]
-
-
-def rearrange_bits(num: int, new_bit_order: List[int]):
-    sig_bits = len(new_bit_order)
-    bit_string = int_to_bit_string(num, sig_bits)
-    result = int_from_bit_string(''.join(bit_string[b] for b in new_bit_order))
-    return result
-
-
-def int_to_bits(num: int, sig_bits: int) -> str:
-    return format(num, '#0%sb' % np.power(2, sig_bits))
-
-
-def int_from_bit_string(bits: str) -> int:
-    return int(bits, 2) if bits.startswith('0b') else int('0b' + bits, 2)
-
-
-def int_to_bit_string(i: int, sig_bits: int) -> str:
-    return '{0:b}'.format(i).zfill(sig_bits)
-
-
-def has_common_qbits(gate1, gate2):
-    return len(set(gate1.qubits).intersection(set(gate2.qubits))) > 0
---
+++ shor/providers/__init__.py
@@ -0,0 +1,3 @@
+from .base import Job, Provider, Result
+from .IBMQ import IBMQJob, IBMQProvider, IBMQResult
+from .ShorSimulator import ShorSimulatorJob, ShorSimulatorResult, ShorSimulatorResult
